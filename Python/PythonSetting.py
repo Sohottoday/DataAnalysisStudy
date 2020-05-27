f85563f207521e4d4849fa1544b131d5f74cb645
Sohottoday <58559786+Sohottoday@users.noreply.github.com>
2020-05-27 21:37:38 +0900

    20200527

---
+++ Python/PythonSetting.py
@@ -0,0 +1,8 @@
+# Anaconda Python : 과학계산에 특화된 파이썬 배포판
+# 공식 배포판 파이썬을 제거한 뒤 아나콘다 파이썬을 설치해야 한다.
+# 환경변수 옵션 체크
+# Anaconda -> Individual Edition -> download -> 자신의 버전에 맞는 것 다운로드
+# All user 체크 -> 왠만해선 C드라이브 바로 아래 설치 -> 환경변수를 설정하겠다는것에 꼭 체크한 뒤 설치
+#
+
+# Jupyter Notebook : 웹 브라우저를 통해 파이썬을 실행하고 다양한 기능 제공
