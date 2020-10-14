52a17af89099c41d94b828d9234bc9b26d581db7
Sohottoday <58559786+Sohottoday@users.noreply.github.com>
2020-10-15 06:29:57 +0900

    tweet key delete

--- Python/PythonSample/avengerstweet.py
+++ Python/PythonSample/avengerstweet.py
@@ -2,10 +2,10 @@ import tweepy
 import pandas as pd
 import os
 
-consumer_key = 'Mwo8KsPPsUH2a2RAuBAGqhOtD'
-consumer_secret = 'uwaZjK3nNCZjUbkUiMRLNsrYH9P0yo6zt2VbE2Ctgk08QtGqZU'
-twitter_access_token = '1311709615923445760-Khswk7BPmSD4GFUDdp2EGlGAZJAjIv'
-twitter_access_secret = 'DD1rtTErJUOfVGjfjsL5ahDKm0ENy9IVxM4BLno0Cshoa'
+consumer_key = ''
+consumer_secret = ''
+twitter_access_token = ''
+twitter_access_secret = ''
 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
--- Python/PythonSample/twittertest.py
+++ Python/PythonSample/twittertest.py
@@ -2,10 +2,10 @@ import tweepy
 import pandas as pd
 import os
 
-consumer_key = 'Mwo8KsPPsUH2a2RAuBAGqhOtD'
-consumer_secret = 'uwaZjK3nNCZjUbkUiMRLNsrYH9P0yo6zt2VbE2Ctgk08QtGqZU'
-twitter_access_token = '1311709615923445760-Khswk7BPmSD4GFUDdp2EGlGAZJAjIv'
-twitter_access_secret = 'DD1rtTErJUOfVGjfjsL5ahDKm0ENy9IVxM4BLno0Cshoa'
+consumer_key = ''
+consumer_secret = ''
+twitter_access_token = ''
+twitter_access_secret = ''
 
 #twitter_api = twitter.Api(consumer_key=twitter_consumer_key, consumer_secret=twitter_consumer_secret, access_token_key=twitter_access_token, access_token_secret=twitter_consumer_secret)
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
