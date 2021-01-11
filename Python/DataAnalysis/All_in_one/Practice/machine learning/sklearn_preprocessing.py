9e27cfdd3a8767ff70902d5d94cd30bc8b732cc8
Sohottoday <58559786+Sohottoday@users.noreply.github.com>
2021-01-11 16:20:15 +0900

    20210111

--- Python/DataAnalysis/All_in_one/Practice/machine learning/sklearn_preprocessing.py
+++ Python/DataAnalysis/All_in_one/Practice/machine learning/sklearn_preprocessing.py
@@ -101,4 +101,50 @@ imputer = SimpleImputer(strategy='most_frequent')           # most_frequent : �
 result = imputer.fit_transform(train[['Embarked', 'Cabin']])
 
 
+# Label Encoding : 문자(categorical)를 수치(numerical)로 변환
+## 학습을 위해서 모든 문자로된 데이터는 수치로 변환하여야 한다.
+def convert(data):
+    if data == 'male':
+        return 1
+    elif data == 'female':
+        return 0
+
+train['Sex'].apply(convert)
+
+## 위의 과정 필요 없이 sklearn의 LabelEncoder를 활용해 쉽게 변환할 수 있다.
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+train['Sex_num'] = le.fit_transform(train['Sex'])      # 라벨 인코딩을 할 column을 학습시키고 변환시킨다. 학습시킨 데이터는 'Sex_num'이라는 새 항목에 담았음
+print(train['Sex_num'].value_counts())      ## 남 녀 숫자데이터로 변환된 것을 확인할 수 있다.
+print(le.classes_)      ## 변환된 클래스들을 확인해보기
+print(le.inverse_transform([0, 1, 1, 0, 0, 1, 1]))           ## 다시 원래의 데이터로 돌리고 싶을때에는 inverse_transform을 활용할 수 있다.
+
+## NaN 값이 포함되어 있다면, LabelEncorder가 제대로 작동하지 않는다.
+## 이러한 경우 결측치 처리를 먼저 해준 뒤 작업한다.
+train['Embarked'] = train['Embarked'].fillna('S')
+le.fit_transform(train['Embarked'])
+
+
+# 원 핫 인코딩(One Hot Encoding)
+print("---------one hot encoding-----------")
+train = pd.read_csv('train.csv')
+
+## Embarked를 원 핫 인코딩 해보려 한다.
+print(train['Embarked'].value_counts())
+train['Embarked'] = train['Embarked'].fillna('S')
+train['Embarked_num'] = LabelEncoder().fit_transform(train['Embarked'])
+print(train['Embarked_num'].value_counts())
+
+## Embarked는 탑승 항구의 이니셜을 나타낸다.
+## 그런데, 우리는 LabelEncoder를 통해 수치형으로 변환해주었다.
+## 하지만, 이대로 데이터를 기계학습 시키면 기계는 데이터 안에서 관계를 학습한다.
+## 즉 'S'=2, 'Q'=1 이라고 되어 있는데 Q+Q=S 가 된다 라고 학습한다.
+## 그렇기 때문에, 독립적인 데이터는 별도의 column으로 분리하고, 각각의 컬럼에 해당 값에만 True 나머지는 False를 갖는다. 이것을 '원 핫 인코딩 한다' 라고 한다.
+
+one_hot = pd.get_dummies(train['Embarked_num'])
+one_hot.columns = ['C', 'Q', 'S']
+print(one_hot)
+## 위와 같이 column을 분리시켜 카테고리형->수치형으로 변환하면서 생기는 수치형 값의 관계를 끊어주면서 독립적인 형태로 바꾸어 준다.
+## 원핫인코딩은 카테고리(계절, 항구, 성별, 종류, 한식/일식/중식...)의 특성을 가지는 column에 대해서 적용한다.
 
