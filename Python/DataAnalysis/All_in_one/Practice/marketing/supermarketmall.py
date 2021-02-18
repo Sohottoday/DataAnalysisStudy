4061ba471b791c803f912d26d2fa038bb5a7d114
Sohottoday <58559786+Sohottoday@users.noreply.github.com>
2021-02-18 22:30:23 +0900

    20210218

--- Python/DataAnalysis/All_in_one/Practice/marketing/supermarketmall.py
+++ Python/DataAnalysis/All_in_one/Practice/marketing/supermarketmall.py
@@ -73,3 +73,47 @@ plt.show()
 # 성별에 따른 boxplot
 sns.boxenplot(x='Gender', y='Age', hue='Gender', palette=['m', 'g'], data=df)
 plt.show()
+
+
+# 데이터 분석
+"""
+# K-means를 사용한 클러스터링
+K-means는 가장 빠르고 단순한 클러스터링 방법 중 한 가지 입니다.
+scikit-learn의 cluster 서브패키지 KMeans 클래스를 사용한다.
+- n_clusters : 군집의 갯수(default = 8)
+- init : 초기화 방법 'random'이면 무작위, 'k-means++' 이면 K-평균++ 방법.(default=k-means++)
+- n_init : centroid seed 시도 횟수. 무작위 중심위치 목록 중 가장 좋은 값을 선택한다.(default=10)
+- max_iter : 최대 반복 횟수(default=300)
+- random_state : 시드값(default=None)
+"""
+
+## Age & spending Score 두 가지 변수를 사용한 클러스터링
+from sklearn.cluster import KMeans
+
+### X1에 'Age', 'Spending Score (1-100)' 의 값을 넣어준다.
+X1 = df[['Age', 'Spending Score (1-100)']].values
+print(X1[:10])
+
+### inertia(관성(응집도))라는 빈 리스트를 만들어준다.
+inertia = []
+
+### 군집수 n을 1에서 11까지 돌아가며 X1에 대해 k-means++ 알고리즘을 적용하여 inertia를 리스트에 저장한다.
+for n in range(1, 11):
+    algorithm = (KMeans(n_clusters=n))
+    algorithm.fit(X1)
+    inertia.append(algorithm.inertia_)
+
+print(inertia)
+# 위 inertia 결과값은 n_clusters 값이 1, 2, 3, 4 등의 값일 때의 관성(응집도) 값
+"""
+# Inertia value를 이용한 적정 k 선택
+- 관성(inertia)에 기반하여 n 개수를 선택한다.
+- 관성(inertia) : 각 중심점(centroid)에서 군집 내 데이터간의 거리를 합산한 것으로 군집의 응집도를 나타낸다. 이 값이 작을수록 응집도가 높은 군집화이다.
+    즉, 작을 수록 좋은 값
+"""
+plt.figure(1, figsize = (16, 5))
+plt.plot(np.arange(1, 11), inertia, 'o')
+plt.plot(np.arange(1, 11), inertia, '-', alpha=0.8)
+plt.xlabel('Number of Clusters'), plt.ylabel('Inertia')
+plt.show()
+# 위 그래프를 보면 엘보 포인트가 4인것을 알 수 있다. 즉, k의 개수는 4개면 적절해 보인다.
