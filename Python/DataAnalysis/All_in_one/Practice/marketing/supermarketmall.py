c1c1bdd87e5b530b03d88eed6e340f39e92c36e1
Sohottoday <58559786+Sohottoday@users.noreply.github.com>
2021-02-20 21:29:27 +0900

    20210220

--- Python/DataAnalysis/All_in_one/Practice/marketing/supermarketmall.py
+++ Python/DataAnalysis/All_in_one/Practice/marketing/supermarketmall.py
@@ -117,3 +117,89 @@ plt.plot(np.arange(1, 11), inertia, '-', alpha=0.8)
 plt.xlabel('Number of Clusters'), plt.ylabel('Inertia')
 plt.show()
 # 위 그래프를 보면 엘보 포인트가 4인것을 알 수 있다. 즉, k의 개수는 4개면 적절해 보인다.
+
+# 군집수를 4로 지정하여 시각화해보기
+algorithm = (KMeans(n_clusters = 4 ,init='k-means++', n_init = 10 ,max_iter=300, 
+                        tol=0.0001,  random_state= 111  , algorithm='elkan') )
+algorithm.fit(X1)
+labels1 = algorithm.labels_
+centroids1 = algorithm.cluster_centers_
+
+h = 0.02
+x_min, x_max = X1[:, 0].min() - 1, X1[:, 0].max() + 1
+y_min, y_max = X1[:, 1].min() - 1, X1[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+Z = algorithm.predict(np.c_[xx.ravel(), yy.ravel()]) 
+
+plt.figure(1 , figsize = (15 , 7) )
+plt.clf()
+Z = Z.reshape(xx.shape)
+plt.imshow(Z , interpolation='nearest', 
+           extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+           cmap = plt.cm.Pastel2, aspect = 'auto', origin='lower')
+
+plt.scatter( x = 'Age' ,y = 'Spending Score (1-100)' , data = df , c = labels1 , 
+            s = 200 )
+plt.scatter(x = centroids1[: , 0] , y =  centroids1[: , 1] , s = 300 , c = 'red' , alpha = 0.5)
+plt.ylabel('Spending Score (1-100)') , plt.xlabel('Age')
+plt.show()
+
+"""
+연령-소비점수를 활용한 군집 4개는 아래와 같이 명명할 수 있다.
+    저연령-고소비 군
+    저연령-중소비 군
+    고연령-중소비 군
+    저소비 군
+군집별 활용 전략 예시
+    이 수퍼마켓mall의 경우 소비점수가 높은 고객들은 모두 40세 이하의 젊은 고객이다.
+    소비점수가 높은 고객들은 연령대가 비슷한 만큼 비슷한 구매패턴과 취향을 가질 가능성이 높다.
+    해당 군집의 소비자 특성을 더 분석해본 뒤 해당 군집의 소비자 대상 VIP 전략을 수립해본다.
+    소비점수가 중간정도인 고객들에게는 연령에 따라 두 개 집단으로 나눠서 접근해본다.
+    소비점수가 낮은 고객군은 연령대별로 중소비점수 군집에 편입될 수 있도록 접근해본다.
+"""
+
+# 'Annual Income (k$)', 'Spending Score (1-100)' 두 가지 변수를 사용한 클러스터링
+## 앞서 예측했던 대로 소득과 소비점수를 활용해 결과를 도출해보자
+# X2에 값을 넣어준다.
+X2 = df[['Annual Income (k$)', 'Spending Score (1-100)']].values
+
+# inertia 라는 빈 리스트를 만든다.
+inertia = []
+
+# 군집수 n을 1에서 11까지 돌아가며 X2에 대해 k-means++ 알고리즘을 적용하여 inertia를 리스트에 저장한다.
+for n in range(1, 11):
+    algorithm = (KMeans(n_clusters=n))
+    algorithm.fit(X2)
+    inertia.append(algorithm.inertia_)
+
+plt.figure(1, figsize = (16, 5))
+plt.plot(np.arange(1, 11), inertia, 'o')
+plt.plot(np.arange(1, 11), inertia, '-', alpha=0.8)
+plt.xlabel('Number of Clusters'), plt.ylabel('Inertia')
+plt.show()
+
+# 군집수를 5로 지정하여 시각화
+algorithm = (KMeans(n_clusters = 5 ,init='k-means++', n_init = 10 ,max_iter=300, 
+                        tol=0.0001,  random_state= 111  , algorithm='elkan') )
+algorithm.fit(X2)
+labels2 = algorithm.labels_
+centroids2 = algorithm.cluster_centers_
+
+h = 0.02
+x_min, x_max = X2[:, 0].min() - 1, X2[:, 0].max() + 1
+y_min, y_max = X2[:, 1].min() - 1, X2[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+Z2 = algorithm.predict(np.c_[xx.ravel(), yy.ravel()]) 
+
+plt.figure(1 , figsize = (15 , 7) )
+plt.clf()
+Z2 = Z2.reshape(xx.shape)
+plt.imshow(Z2 , interpolation='nearest', 
+           extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+           cmap = plt.cm.Pastel2, aspect = 'auto', origin='lower')
+
+plt.scatter( x = 'Annual Income (k$)' ,y = 'Spending Score (1-100)' , data = df , c = labels2 , 
+            s = 200 )
+plt.scatter(x = centroids2[: , 0] , y =  centroids2[: , 1] , s = 300 , c = 'red' , alpha = 0.5)
+plt.ylabel('Spending Score (1-100)') , plt.xlabel('Annual Income (k$)')
+plt.show()
