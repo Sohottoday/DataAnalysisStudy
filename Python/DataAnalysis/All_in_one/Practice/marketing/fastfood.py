16134aba71937330107fb5553a48d0e7bb62fe1f
Sohottoday <58559786+Sohottoday@users.noreply.github.com>
2021-03-25 23:54:50 +0900

    20210325

---
+++ Python/DataAnalysis/All_in_one/Practice/marketing/fastfood.py
@@ -0,0 +1,28 @@
+import warnings
+
+warnings.filterwarnings('ignore')
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
+import missingno
+import folium
+import numpy as np
+import glob
+
+import matplotlib.font_manager as fm
+
+path = 'C:\\Users\\user\\AppData\\Local\\Microsoft\\Windows\\Fonts\\NanumSquareRoundEB.ttf'
+font_name = fm.FontProperties(fname=path, size=14).get_name()
+print(font_name)
+plt.rc('font', family=font_name)
+
+# 설정한 폰트는 rebuild 해줘야 적용된다.
+fm._rebuild()
+
+df_store = pd.read_csv('C:\\Users\\user\\Desktop\\Programing\\Github\\SelfStudy\\Python\\DataAnalysis\\All_in_one\\Practice\\marketing\\소상공인시장진흥공단_상가(상권)정보_경남_202012.csv', encoding='utf-8', delimiter='|')
+
+print(df_store.shape)
+print(df_store.columns)
+
