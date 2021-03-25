216afb8e03c280a84101d18b16e906f9bf9616ed
Sohottoday <58559786+Sohottoday@users.noreply.github.com>
2021-03-25 23:59:00 +0900

    20210325

--- Python/DataAnalysis/All_in_one/Practice/marketing/fastfood.py
+++ Python/DataAnalysis/All_in_one/Practice/marketing/fastfood.py
@@ -21,7 +21,7 @@ plt.rc('font', family=font_name)
 # 설정한 폰트는 rebuild 해줘야 적용된다.
 fm._rebuild()
 
-df_store = pd.read_csv('C:\\Users\\user\\Desktop\\Programing\\Github\\SelfStudy\\Python\\DataAnalysis\\All_in_one\\Practice\\marketing\\소상공인시장진흥공단_상가(상권)정보_경남_202012.csv', encoding='utf-8', delimiter='|')
+df_store = pd.read_csv('C:\\Users\\user\\Desktop\\Programing\\Github\\SelfStudy\\Python\\DataAnalysis\\All_in_one\\Practice\\marketing\\소상공인시장진흥공단_상가(상권)정보_제주_202012.csv', encoding='utf-8', delimiter='|')
 
 print(df_store.shape)
 print(df_store.columns)
