ee49483f5212bfe25f6c5b803fae47225c3c49bd
Sohottoday <58559786+Sohottoday@users.noreply.github.com>
2021-04-10 22:47:31 +0900

    20210410

--- Python/DataAnalysis/All_in_one/Practice/timeSeries/risk.py
+++ Python/DataAnalysis/All_in_one/Practice/timeSeries/risk.py
@@ -40,3 +40,79 @@ ex) 상품 A : 수익률 10%, 변동성 10%
 따라서 리스크를 고려했을 경우 5:5 투자가 더 수익을 얻을 확률이 높다.
 이러한 방식으로 포트폴리오를 분석하여 투자한다.
 """
+
+import pandas as pd
+import numpy as np
+import yfinance as yf       # 금융정보를 가져올 수 있는 라이브러리
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+"""
+# 종목코드 예시
+- 미국기업 : 'MSFT'
+- 한국기업 : '005930.KS'    (삼성)
+- 인덱스 : '^KS11'
+
+# 포트폴리오 평가
+- 여러 조합으로 자산 포트폴리오를 구성한 뒤에 Return, Risk, Sharpe Ratio 비교 평가
+- 미국 Tech기업, 한국의 Tech기업의 투자 포트폴리오 비교해보기
+"""
+
+# 데이터 가져오기, 시각화
+Tech_US = ['MSFT', 'NFLX', 'FB', 'AMZN']    # 마이크로소프트, 넷플릭스, 페이스북, 아마존
+Tech_KR = ['005930.KS', '000660.KS', '035420.KS', '035720.KS']  # 삼성, SK하이닉스, 네이버, 카카오
+
+print(yf.Ticker('MSFT').history(start='2019-04-01', end='2021-03-31'))     # 한달동안의 정보 / start, end로 기간을 지정할 수 있다.
+
+# price(종가), dividends(배당)을 가져오는 함수를 정의
+def get_price(companies):
+    df = pd.DataFrame()
+    for company in companies:
+        df[company] = yf.Ticker(company).history(start='2019-04-01', end='2021-03-31')['Close']
+    return df
+
+def get_div(companies):
+    df = pd.DataFrame()
+    for company in companies:
+        df[company] = yf.Ticker(company).history(start='2019-04-01', end='2021-03-31')['Dividends']
+    return df
+
+p_US = get_price(Tech_US)
+print(p_US)
+
+p_KR = get_price(Tech_KR)
+p_KR.columns = ['SS', 'SKH', 'NVR', 'KKO']
+
+d_US = get_div(Tech_US)
+d_KR = get_div(Tech_KR)
+d_KR.columns = ['SS', 'SKH', 'NVR', 'KKO']
+# 2년간 기업당 배당금
+print(d_US.sum())
+print(d_KR.sum())
+
+# 시각화
+## 최초 가격 대비 변동률
+(p_US/p_US.iloc[0]).plot(figsize=(15, 5))
+plt.show()
+
+(p_KR/p_KR.iloc[0]).plot(figsize=(15, 5))
+plt.show()
+
+# Daily Return
+r_US = p_US / p_US.shift() - 1
+r_KR = p_KR / p_KR.shift() - 1
+
+# Average Return(Total period)
+# 2년 평균 수입률
+r_a_US = (p_US.iloc[-1] + d_US.sum()) / p_US.iloc[0] - 1
+print(r_a_US)
+
+r_a_KR = (p_KR.iloc[-1] + d_KR.sum()) / p_KR.iloc[0] - 1
+print(r_a_KR)
+
+# Average Return(Daily)
+r_a_d_US = (1+r_a_US)**(1/p_US.shape[0])-1
+print(r_a_d_US)
+
+r_a_d_KR = (1+r_a_KR)**(1/p_KR.shape[0])-1
+print(r_a_d_KR)
